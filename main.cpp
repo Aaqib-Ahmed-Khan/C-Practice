a2816f1966bc5906dd99dd17897239f864951089
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-10-26 22:47:57 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "\t\t\t\t\t\Hello World" << std::endl;  // 5 tabs and a backslash before "Hello World"
+    return 0;
+}
+
---
+++ main1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Pakistan";
+    return 0; // It's good practice to return 0 at the end of main
+}
+
